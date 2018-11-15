a870dfa72b3c09bd935216dd256e73b34ccd168d
MigueMartelo <miguemartelo1031@gmail.com>
2018-11-15 14:53:54 -0500

    Agregando componente para Navegacion

---
+++ src/components/Navegacion.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navegacion = () => {
+    return ( 
+        <div className="py-5">
+            <button className="btn btn-info mr-1">&larr; Anterior </button>
+            <button className="btn btn-info ">Siguiente &rarr;</button>
+        </div>
+    );
+}
+ 
+export default Navegacion;
